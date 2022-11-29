573297642d33f49db8345fdd93993afb9e03c1b7
jose1678 <alfredosalazarg@yahoo.com.ar>
2022-11-29 09:54:31 -0300

    clase JS-III

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,54 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    
+    var suma = 0;
+    for (var i = 0 ; i < 11 ; i++) {
+     suma = suma + i;
+    }
+    return suma
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+ return array.filter((elemento)=> elemento %2 === 0);
+ 
+
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+var cuadrado = array.map((elemento)=> elemento ** 2);
+return cuadrado;
+  
+  
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((acumulador,siguiente)=> acumulador + siguiente);
+  
+  var suma=0;
+  for(var i = 0; i< array.length; i++){
+    suma= suma + array[i];
+  }
+  return suma;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length
+  
+ var numeroEnString = num.toString();
+ var longitud = numeroEnString.length;
+ return longitud; 
 }
   
   
